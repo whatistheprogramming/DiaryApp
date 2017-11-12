b901106b4948bb9062f785ff61e64d0264272353
yuan <491187718@qq.com>
2017-11-12 13:30:40 +0800

    First commit

---
+++ app/src/main/java/com/example/yuan/demodiaryapp/entity/Diary.java
@@ -0,0 +1,93 @@
+package com.example.yuan.demodiaryapp.entity;
+
+/**
+ * Created by yuan on 2017/11/12.
+ */
+
+public class Diary
+{
+    //id
+    private int diaryId;
+    //作者
+    private String author;
+    //标题
+    private String title;
+    //日期
+    private String date;
+    //地址
+    private String address;
+    //保存内容的路径
+    private String uri;
+    //内容
+    private String content;
+
+    public int getDiaryId()
+    {
+        return diaryId;
+    }
+
+    public void setDiaryId(int diaryId)
+    {
+        this.diaryId = diaryId;
+    }
+
+    public String getAuthor()
+    {
+        return author;
+    }
+
+    public void setAuthor(String author)
+    {
+        this.author = author;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getDate()
+    {
+        return date;
+    }
+
+    public void setDate(String date)
+    {
+        this.date = date;
+    }
+
+    public String getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(String address)
+    {
+        this.address = address;
+    }
+
+    public String getUri()
+    {
+        return uri;
+    }
+
+    public void setUri(String uri)
+    {
+        this.uri = uri;
+    }
+
+    public String getContent()
+    {
+        return content;
+    }
+
+    public void setContent(String content)
+    {
+        this.content = content;
+    }
+}
